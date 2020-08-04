336156d9b2ad8112bc7084e45484e079b7364720
Lineweb <69199669+Lineweb@users.noreply.github.com>
2020-08-04 14:28:38 +0200

    Add files via upload

---
+++ enforme/js/style.js
@@ -0,0 +1,8 @@
+//Popup formulaire
+
+function openModal() {
+    document.getElementById("modal").style.top="5%";
+}
+function closeModal() {
+    document.getElementById("modal").style.top="-100%";
+}
